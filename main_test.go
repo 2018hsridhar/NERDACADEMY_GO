09bec569cbc9403d18dba6e7efa84438c623b15c
Hari Sridhar <hari.h.sridhar@gmail.com>
2023-07-07 15:48:49 -0700

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+Test server behavior
+Behaviors to test
+(a) Server starts up
+(b) if server fails, we see expected log message
+(c) if a server does not respond with the expected value
+(d)
+(e)
+
+*/
